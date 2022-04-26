0dc048ef3a5202e62cb728151d1f0f5a361ca539
salem z <salem.zerezghi2@gmail.com>
2022-04-26 12:45:13 -0400

    made some API requests

---
+++ assets/js/homepage.js
@@ -0,0 +1,23 @@
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+     
+    });
+  });
+  
+};
+//getUserRepos("salemiana");
+
+//   fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//     response.json().then(function(data) {
+//     console.log(data);
+  
+//   });
+// });
+  
+//   console.log("outside");
